5d208ace8fbc71c4d8eda07eb415c30db560f8b3
Barrett Golding <bgMBP@BG-MBP.local>
2017-05-06 09:58:42 -0600

    Add get/set option (and option items) functions.

--- sound-shares.php
+++ sound-shares.php
@@ -29,6 +29,12 @@ if ( defined( 'SOUNDSHARES_VERSION' ) ) {
 
 define( 'SOUNDSHARES_VERSION', '0.1.0' );
 
+/*
+Set vars, inc. fb_app_id, fb_admins.
+Check xml namespace.
+Add xml
+
+*/
 
 /**
  * Variables for Open Graph tags.
@@ -190,6 +196,232 @@ function soundshares_check_url() {
     } else {
 
     }
+}
+
+
+
+//Add Open Graph Meta Info from the actual article data, or customize as necessary
+function soundshares_og() {
+    global $post;
+    $post_id = $post->ID;
+    $og_meta = array(); // Clear var.
+
+    // Get post excerpt for og:descritpion value.
+    if ( $excerpt = $post->post_excerpt) {
+        $excerpt = strip_tags( $post->post_excerpt );
+        $excerpt = str_replace("", "'", $excerpt);
+    } else {
+        $excerpt = get_bloginfo('description');
+    }
+
+    // Get post featured image URL for og:image value.
+    if ( has_post_thumbnail( $post_id ) ) {
+        $image_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'large' );
+        $og_image = $image_src[0];
+    } else { // Use default image.
+        $og_image = soundshares_get_option( 'og_image ');
+    }
+
+    $og_meta['og:title']       = get_the_title();
+    $og_meta['og:description'] = $og_excerpt;
+    $og_meta['og:url']         = get_permalink();
+    $og_meta['og:site_name']   = get_bloginfo('name');
+    $og_meta['og:image']       = $og_image;
+
+    // <meta property="og:title" content="esc_attr({Title})"/>
+
+    return $og_meta;
+}
+
+/* ------------------------------------------------------------------------ *
+ * Functions to get/set options array.
+ * ------------------------------------------------------------------------ */
+
+/**
+ * Retrieves an option, and array of plugin settings, from database.
+ *
+ * Option functions based on Jetpack Stats:
+ * @link https://github.com/Automattic/jetpack/blob/master/modules/stats.php
+ *
+ * @since   0.1.0
+ *
+ * @uses    soundshares_upgrade_options()
+ * @return  array   $options    Array of plugin settings
+ */
+function soundshares_get_options() {
+    $options = get_option( 'soundshares' );
+
+    // Set version if not the latest.
+    if ( ! isset( $options['version'] ) || $options['version'] < SOUNDSHARES_VERSION ) {
+        $options = soundshares_upgrade_options( $options );
+    }
+
+    return $options;
+}
+
+/**
+ * Makes array of plugin settings, merging default and new values.
+ *
+ * @since   0.1.0
+ *
+ * @uses    soundshares_set_options()
+ * @param   array   $options        Array of plugin settings
+ * @return  array   $new_options    Merged array of plugin settings
+ */
+function soundshares_upgrade_options( $options ) {
+// Get hostname (e.g., 'example.com') from site URL.
+    $site_url = site_url();
+    $site_host = parse_url( $site_url, PHP_URL_HOST );
+
+    $defaults = array(
+        'user_roles' => array( 'administrator' ),
+        'post_types' => array( 'post' ),
+        'allow'      => array(
+            'urls_script'   => '1',
+            'urls_style'    => '1',
+            'class_body'    => 'on',
+            'class_post'    => 'on',
+        ),
+        'url_whitelist' => $site_host,
+    );
+
+    if ( is_array( $options ) && ! empty( $options ) ) {
+        $new_options = array_merge( $defaults, $options );
+    } else {
+        $new_options = $defaults;
+    }
+
+    $new_options['version'] = SOUNDSHARES_VERSION;
+
+    soundshares_set_options( $new_options );
+
+    return $new_options;
+}
+
+/**
+ * Sets an option in database (an array of plugin settings).
+ *
+ * Note: update_option() adds option if it doesn't exist.
+ *
+ * @since   0.1.0
+ *
+ * @param   array   $option     Array of plugin settings
+ */
+function soundshares_set_options( $options ) {
+    $options_clean = soundshares_sanitize_data( $options );
+    update_option( 'soundshares', $options_clean );
+}
+
+/**
+ * Sanitizes values in an one- and multi- dimensional arrays.
+ *
+ * Used by post meta-box form before writing post-meta to database
+ * and by Settings API before writing option to database.
+ *
+ * @link https://tommcfarlin.com/input-sanitization-with-the-wordpress-settings-api/
+ *
+ * @since    0.4.0
+ *
+ * @param    array    $input        The address input.
+ * @return   array    $input_clean  The sanitized input.
+ */
+function soundshares_sanitize_data( $data = array() ) {
+    // Initialize a new array to hold the sanitized values.
+    $data_clean = array();
+
+    // Check for non-empty array.
+    if ( ! is_array( $data ) || ! count( $data )) {
+        return array();
+    }
+
+    // Traverse the array and sanitize each value.
+    foreach ( $data as $key => $value) {
+        // For one-dimensional array.
+        if ( ! is_array( $value ) && ! is_object( $value ) ) {
+            // Remove blank lines and whitespaces.
+            $value = preg_replace( '/^\h*\v+/m', '', trim( $value ) );
+            $value = str_replace( ' ', '', $value );
+            $data_clean[ $key ] = sanitize_text_field( $value );
+        }
+
+        // For multidimensional array.
+        if ( is_array( $value ) ) {
+            $data_clean[ $key ] = soundshares_sanitize_data( $value );
+        }
+    }
+
+    return $data_clean;
+}
+
+/**
+ * Sanitizes values in an one-dimensional array.
+ * (Used by post meta-box form before writing post-meta to database.)
+ *
+ * @link https://tommcfarlin.com/input-sanitization-with-the-wordpress-settings-api/
+ *
+ * @since    0.4.0
+ *
+ * @param    array    $input        The address input.
+ * @return   array    $input_clean  The sanitized input.
+ */
+function soundshares_sanitize_array( $input ) {
+    // Initialize a new array to hold the sanitized values.
+    $input_clean = array();
+
+    // Traverse the array and sanitize each value.
+    foreach ( $input as $key => $val ) {
+        $input_clean[ $key ] = sanitize_text_field( $val );
+    }
+
+    return $input_clean;
+}
+
+function soundshares_remove_empty_lines( $string ) {
+    return preg_replace( "/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string );
+    // preg_replace( '/^\h*\v+/m', '', $string );
+}
+
+/* ------------------------------------------------------------------------ *
+ * Functions to get/set a specific options array item.
+ * ------------------------------------------------------------------------ */
+
+/**
+ * Retrieves a specific setting (an array item) from an option (an array).
+ *
+ * @since   0.1.0
+ *
+ * @uses    soundshares_get_options()
+ * @param   array|string    $option     Array item key
+ * @return  array           $option_key Array item value
+ */
+function soundshares_get_option( $option_key = NULL ) {
+    $options = soundshares_get_options();
+
+    // Returns valid inner array key ($options[$option_key]).
+    if ( isset( $options ) && $option_key != NULL && isset( $options[ $option_key ] ) ) {
+            return $options[ $option_key ];
+    } else { // Inner array key not valid.
+    return NULL;
+    }
+}
+
+/**
+ * Sets a specified setting (array item) in the option (array of plugin settings).
+ *
+ * @since   0.1.0
+ *
+ * @uses    soundshares_set_options()
+ *
+ * @param   string  $option     Array item key of specified setting
+ * @param   string  $value      Array item value of specified setting
+ * @return  array   $options    Array of plugin settings
+ */
+function soundshares_set_option( $option, $value ) {
+    $options = soundshares_get_options();
+
+    $options[$option] = $value;
+
+    soundshares_set_options( $options );
 }
 
 /**
